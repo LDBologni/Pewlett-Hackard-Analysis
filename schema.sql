b4281c57c5b8c20f5f6c4c2c95361622d9364e1f
Leonardo Bologni <lbologni_05@hotmail.com>
2022-04-03 21:34:08 -0400

    Uploading Retirement CSV

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- Creating tables for PH-EmployeeDB
+-- Create tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
--- Creating Employees Table
+-- Create Employees Table
 CREATE TABLE employees (
      emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
@@ -36,6 +36,27 @@ CREATE TABLE salaries (
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
+);
+
+--Create Dept Employees Table
+CREATE TABLE dept_emp (
+emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create Titles Table
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
